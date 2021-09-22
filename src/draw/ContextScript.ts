719c1de4b730a2f156d2bb708751c8625c0b98ae
yggdrasil-git-user <jerrymatthewmorris@icloud.com>
2021-09-21 20:33:41 -0600

    posable eyes

---
+++ src/draw/ContextScript.ts
@@ -0,0 +1,8 @@
+import { NodeCanvasRenderingContext2D } from "canvas"
+import { DarumaCanvas } from "../daruma-canvas"
+
+class ContextScript extends DarumaCanvas{
+    constructor(context: NodeCanvasRenderingContext2D){
+        super()
+    }
+}
